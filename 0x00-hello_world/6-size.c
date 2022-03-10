0996ee517841aed817f3c11fc83c47d5a1fadbb0
AdamboJnr <AdamboAllan75@gmail.com>
2022-03-10 13:18:06 -0800

    display sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: \n", sizeof(char));
-	printf("Size of an int: \n", sizeof(int));
-	printf("Size of a long int: \n", sizeof(long int));
-	printf("Size of a long long int: \n", sizeof(long long int));
-	printf("Size of a float: \n", sizeof(float));
+	printf("Size of a char: %d \n", sizeof(char));
+	printf("Size of an int: %d \n", sizeof(int));
+	printf("Size of a long int: %d \n", sizeof(long int));
+	printf("Size of a long long int: %d \n", sizeof(long long int));
+	printf("Size of a float: %d \n", sizeof(float));
 	return (0);
 }
 
